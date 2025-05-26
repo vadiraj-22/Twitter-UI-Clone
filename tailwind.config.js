32c812db9c5fb6731b5d35587b38ce1dd103b726
vadiraj-22 <154509517+vadiraj-22@users.noreply.github.com>
2025-05-26 18:08:29 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Include your main HTML file
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Include all HTML/JS/TS files in src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
